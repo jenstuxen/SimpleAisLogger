92e75a48e19ef1c7c5c6fb392fdbe55eb690b966
Jens Tuxen <mail@jenstuxen.com>
2013-11-18 22:09:40 +0100

    minor?

--- src/main/java/org/tuxen/simepleais/AisDataMap.java
+++ src/main/java/org/tuxen/simepleais/AisDataMap.java
@@ -8,16 +8,15 @@ import java.util.LinkedHashMap;
  * @author Jens Tuxen
  */
 public class AisDataMap extends LinkedHashMap<String, String> {
-
 	private static final long serialVersionUID = 1L;
 
-	//final static String keys = "MMSI,Ship name,Call sign,imo number,Ship type,destination,Length,Breadth,maximum actual draught,cargo,Latitude,Longitude,Time stamp,SOG,COG,heading,nav status,Long,Lat,Time"
-	//		.toLowerCase();
+	// final static String keys =
+	// "MMSI,Ship name,Call sign,imo number,Ship type,destination,Length,Breadth,maximum actual draught,cargo,Latitude,Longitude,Time stamp,SOG,COG,heading,nav status,Long,Lat,Time"
+	// .toLowerCase();
 
 	final static String keys = "MMSI,Ship name,Call sign,imo number,Ship type,destination,maximum actual draught,cargo,Latitude,Longitude,Time stamp,SOG,COG,heading,nav status,Long,Lat,Time,starboard,port,stern,bow"
 			.toLowerCase();
 
-	
 	public AisDataMap() {
 		super(keys.split(",").length);
 		for (String k : keys.split(",")) {
@@ -34,12 +33,10 @@ public class AisDataMap extends LinkedHashMap<String, String> {
 		super.put(key.toLowerCase(), Double.toString(value));
 	}
 
-	
 	public void put(String key, Long value) {
 		super.put(key.toLowerCase(), Long.toString(value));
 	}
 
-	
 	@Override
 	public String toString() {
 		return this.toTSVString();
@@ -48,11 +45,11 @@ public class AisDataMap extends LinkedHashMap<String, String> {
 	public String toTSVString() {
 		return toString('\t');
 	}
-	
+
 	public String toCSVString() {
 		return toString(',');
 	}
-	
+
 	public String toString(Character seperator) {
 		StringBuilder sb = new StringBuilder();
 		for (String key : this.keySet()) {
@@ -61,5 +58,4 @@ public class AisDataMap extends LinkedHashMap<String, String> {
 		}
 		return sb.toString();
 	}
-
 }
